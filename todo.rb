e41f0ed99f72aee245dbd33c7e305cd753a19bcf
wotd <ngebicki@gmail.com>
2016-04-28 07:22:02 +0200

    empty classes created

---
+++ todo.rb
@@ -0,0 +1,25 @@
+=begin
+List Class:
+  - Create a list
+  - Add task to list
+  - Show all tasks
+  - Read a task from a file
+  - Write a list to a file
+  - Delete a task
+  - Update a task
+
+Task Class:
+  - Create a task item
+=end
+
+#Classes
+class List
+
+end
+
+class Task
+
+end
+
+#Program starts here
+my_list = List.new
